a1c6e7a0da0cbe88909b0e4bc9907a96d1e05376
gweryf <priyam2007sp@gmail.com>
2022-08-16 10:45:52 +0530

    implemented grid and hover settings

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById('canvas');
-
-for(let i = 0;i<16;i++){
+canvas.style.gridTemplateColumns = 'repeat(16,1fr)';
+canvas.style.gridTemplateColumns = 'repeat(16,1fr)';
+for(let i = 0;i<16*16;i++){
     let canvasele = document.createElement('div');
     canvasele.className = 'canvas-element';
+    canvasele.style.background = 'grey';
+    canvasele.addEventListener('mouseover',function(){
+        canvasele.style.background = 'black';
+    });
     canvas.appendChild(canvasele);
 }
