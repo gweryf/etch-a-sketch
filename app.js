52cd2a40b1d3196822ba25d9a81b981f727852d6
gweryf <priyam2007sp@gmail.com>
2022-08-26 23:26:43 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const canvas = document.getElementById('canvas');
+const colorbut = document.getElementById('color');
+const rgbbut = document.getElementById('rgb');
+const eraserbut = document.getElementById('eraser');
+const clearbut = document.getElementById('clear');
+
+let defcolor = 'black';
+
+let r = Math.random*100;
+let g = Math.random*100;
+let b = Math.random*100;
+
+
+
+eraserbut.addEventListener('click',function(e){
+    defcolor = 'grey';
+    // console.log(e);
+});
+
+
+colorbut.addEventListener('click',function(e){
+    defcolor = 'black';
+    // console.log(e);
+});
+
+
 canvas.style.gridTemplateColumns = 'repeat(16,1fr)';
 canvas.style.gridTemplateColumns = 'repeat(16,1fr)';
 for(let i = 0;i<16*16;i++){
@@ -6,7 +31,7 @@ for(let i = 0;i<16*16;i++){
     canvasele.className = 'canvas-element';
     canvasele.style.background = 'grey';
     canvasele.addEventListener('mouseover',function(){
-        canvasele.style.background = 'black';
+        canvasele.style.background = defcolor;
     });
     canvas.appendChild(canvasele);
 }
